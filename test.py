b5809c8b54cc32c99cdd0cb0f3c04a8e43acbc54
Nikita Bernstein <github@nikitab.com>
2011-12-26 19:56:08 -0500

    Testing commitment from GitHub client

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ from libraries import geobox
 
 
 print geobox.compute_tuple( 42.394497, -71.120768, -1, 5 )
-
+   
 print geobox.format_tuple( geobox.compute_tuple( 42.394497, -71.120768, -1, 5 ), -1 )
